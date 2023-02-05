a4a80a9836ef699339e9209ad4503a052b8bb5ad
ahoang928 <ahoang928@gmail.com>
2023-02-04 22:32:40 -0500

    Button to backend

--- pythonWebsite.py
+++ pythonWebsite.py
@@ -1,13 +1,20 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, Blueprint
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template('Homepage draft 1 (simone).html')
+  return render_template('Website.html')
 
 @app.route("/country")
 def countryPage():
-  return render_template('CountryPage.html')
+  return render_template('CountryPageSimone.html')
+
+@app.route("/Food.html", methods=['Get','POST'])
+def translatePage():
+    IMCRYING = request.form
+    print(IMCRYING)
+    return render_template('ContactInfo')
 
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=80)
+  app.run(host='0.0.0.0', port=80,debug=True)
